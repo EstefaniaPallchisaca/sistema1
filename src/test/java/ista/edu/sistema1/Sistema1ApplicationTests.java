aa1bf98db2bd79c7be5b214b47947eb27064aab2
EstefaniaPallchisaca <stefa._95@hotmail.com>
2022-04-06 20:08:45 -0500

    inicio

---
+++ src/test/java/ista/edu/sistema1/Sistema1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ista.edu.sistema1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sistema1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
